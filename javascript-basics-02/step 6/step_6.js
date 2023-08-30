ef31bf4a01c6c18ba8694976c63e2bc81868c823
Hasan Deeb <h.deeb712@gmail.com>
2023-08-30 21:22:22 +0300

    js-2 step6

--- javascript-basics-02/step 6/step_6.js
+++ javascript-basics-02/step 6/step_6.js
@@ -3,8 +3,7 @@ images.forEach((element) => {
     element.addEventListener('mouseover', (e) => {
         e.target.setAttribute('src', `./images/${element.id}_2.jpg`)
         })
-        // ### For getting back initial image on mouse out :)
-        // element.addEventListener('mouseout', (e) =>{
-        //     e.target.setAttribute('src', `./images/${element.id}.jpg`)
-        // }) 
+        element.addEventListener('mouseout', (e) =>{
+            e.target.setAttribute('src', `./images/${element.id}.jpg`)
+        }) 
     })

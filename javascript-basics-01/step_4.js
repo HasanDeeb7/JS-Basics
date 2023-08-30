cdc86fed74e1d9792d022aa397664be9f814ff3e
Hasan Deeb <h.deeb712@gmail.com>
2023-08-30 13:22:27 +0300

    step4

---
+++ javascript-basics-01/step_4.js
@@ -0,0 +1,6 @@
+const name = prompt('Please Enter Your First Name')
+const surname = prompt('Please Enter Your Last Name')
+const city = prompt('Where Do You Live')
+alert(`Name: ${name}
+surname: ${surname}
+city: ${city}`)

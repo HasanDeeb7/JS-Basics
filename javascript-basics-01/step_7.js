087ca38fef214f41222445b386ad7d5b2fd1f3eb
Hasan Deeb <h.deeb712@gmail.com>
2023-08-30 13:42:34 +0300

    Revert "step5"
    This reverts commit e69ecc4f1c075de8107c87f955bad3c7d4634058.

--- javascript-basics-01/step_7.js
+++
@@ -1,7 +0,0 @@
-let birthYear = prompt("Birth Year")
-let shoeSize = prompt('Shoe Size')
-const process = (shoe, birth) =>{
-    const result = (shoe * 2 + 5) * 50 - birth + 1766
-    return result
-}
-alert('Result: ' + process(shoeSize, birthYear))
